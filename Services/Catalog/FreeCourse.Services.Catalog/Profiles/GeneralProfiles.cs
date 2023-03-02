68d26913bd273e76de5d802a28a958f8f899f74c
atascanturk <atascanturk@gmail.com>
2023-03-03 00:34:05 +0300

    Dtos has been added. Profile class has been added. appsettings.json file has been configured.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Profiles/GeneralProfiles.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Profiles
+{
+    public class GeneralProfiles :Profile
+    {
+        public GeneralProfiles()
+        {
+            CreateMap<Course, CourseDto>().ReverseMap();
+            CreateMap<Course, CourseCreateDto>().ReverseMap();
+            CreateMap<Course, CourseUpdateDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Feature, FeatureDto>().ReverseMap();
+        }
+    }
+}
