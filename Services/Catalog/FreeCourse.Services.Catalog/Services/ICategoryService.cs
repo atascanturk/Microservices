e14e7d18e92d5cea51eca3c954f5b30c536c655a
atascanturk <atascanturk@gmail.com>
2023-03-04 17:07:00 +0300

    CourseService and CategoryService has been added.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Models;
+using FreeCourse.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Services
+{
+    interface ICategoryService
+    {
+        public Task<Response<List<CategoryDto>>> GetAllAsync();
+        public  Task<Response<CategoryDto>> CreateAsync(Category category);
+        public Task<Response<CategoryDto>> GetByIdAsync(string id);
+    }
+}
---
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/ICourseService.cs
@@ -0,0 +1,18 @@
+﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Services
+{
+     interface ICourseService
+    {
+        public Task<Response<List<CourseDto>>> GetAllAsync();
+        public Task<Response<CourseDto>> CreateAsync(CourseCreateDto courseCreateDto);
+        public Task<Response<NoContent>> UpdateAsync(CourseUpdateDto courseUpdateDto);
+        public Task<Response<NoContent>> DeleteAsync(string id);
+        public Task<Response<CourseDto>> GetByIdAsync(string id);
+    }
+}
